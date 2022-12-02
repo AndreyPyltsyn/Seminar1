85a69291d326beaa432b5fbcc649ac24ce34d24f
Andrey <andpyl@mail.ru>
2022-12-02 17:07:31 +0300

    Task4+

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите числo");
+string input = Console.ReadLine()!;
+int parseInput = int.Parse(input);
+
+for (int N = 1; N <= parseInput; N++)
+{
+    if (N % 2 == 0)
+    {
+        Console.WriteLine(N);
+    }
+}
